636537a261ed3738d63622c410acc241799f02b5
b3njah <benjahjp@gmail.com>
2023-05-27 14:21:06 +0300

    feat: presentation layer

--- app/src/main/java/com/example/superheroes/presentation/main/components/MainScreen.kt
+++ app/src/main/java/com/example/superheroes/presentation/main/components/MainScreen.kt
@@ -7,21 +7,27 @@ import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowDown
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import coil.compose.AsyncImage
 import com.example.superheroes.domain.model.SuperheroModel
+import com.example.superheroes.presentation.main.MainScreenState
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(
     getNextSuperHero: (id: Int) -> Unit,
-    superheroModel: SuperheroModel
+    superheroModel: SuperheroModel,
+    state: MutableState<MainScreenState>
 ) {
     val scope = rememberCoroutineScope()
     val pagerState = rememberPagerState(
@@ -30,32 +36,40 @@ fun MainScreen(
         pageCount = { 751 }
     )
     Box {
-        VerticalPager(
-            state = pagerState
-        ) {
-            AsyncImage(
-                model = superheroModel.image.url,
-                contentScale = ContentScale.Crop,
-                contentDescription = superheroModel.name
+        if (state.value.isLoading){
+            CircularProgressIndicator(
+                modifier = Modifier.align(
+                    Alignment.Center
+                )
             )
-        }
-        Column {
-            Text(text = superheroModel.name)
-        }
-        IconButton(
-            onClick = {
-                scope.launch {
-                    pagerState.animateScrollToPage(
-                        pagerState.currentPage + 1
-                    )
-                }
-                getNextSuperHero(pagerState.currentPage + 1)
+        } else {
+            VerticalPager(
+                state = pagerState
+            ) {
+                AsyncImage(
+                    model = superheroModel.image.url,
+                    contentScale = ContentScale.Crop,
+                    contentDescription = superheroModel.name
+                )
             }
-        ) {
-            Icon(
-                imageVector = Icons.Rounded.KeyboardArrowDown,
-                contentDescription = "arrow down"
-            )
+            Column {
+                Text(text = superheroModel.name)
+            }
+            IconButton(
+                onClick = {
+                    scope.launch {
+                        pagerState.animateScrollToPage(
+                            pagerState.currentPage + 1
+                        )
+                    }
+                    getNextSuperHero(pagerState.currentPage + 1)
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.KeyboardArrowDown,
+                    contentDescription = "arrow down"
+                )
+            }
         }
     }
 }
